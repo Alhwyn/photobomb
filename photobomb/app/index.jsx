5c512f2976a7f681817d122dd94471d3ef20e54b
alhwyn <alhwyndwyne0411@gmail.com>
2024-12-19 07:59:04 -0800

    popo

--- photobomb/app/index.jsx
+++ photobomb/app/index.jsx
@@ -1,8 +1,7 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
-import ScreenWrapper from '../components/ScreenWrapper'
-import Loading from '../components/Loading'
+
 import Button from '../components/Button'
 
 const index = () => {
