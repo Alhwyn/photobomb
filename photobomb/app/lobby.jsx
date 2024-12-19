9e7b8881fb6bc2968b77a566ec1ec75342667497
alhwyn <alhwyndwyne0411@gmail.com>
2024-12-19 06:07:35 -0800

    added big button

--- photobomb/app/lobby.jsx
+++ photobomb/app/lobby.jsx
@@ -1,17 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Button from '../components/Button'
+import ScreenWrapper from '../components/ScreenWrapper'
+import { StatusBar } from 'expo-status-bar'
+import { wp } from '../helpers/common'
+import { useRouter } from 'expo-router'
 
 
 const lobby = () => {
+    const router = useRouter();
+
   return (
-    <View>
-      <Text>lobby</Text>
-      <Button title="welcome"/>
-    </View>
+    <ScreenWrapper>
+        <StatusBar style='dark' />
+        <View style={styles.container}>
+            <Button
+                title='Hello world'
+            />
+
+        </View>
+
+
+
+    </ScreenWrapper>
   )
 }
 
 export default lobby
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: 'red',
+        marginHorizontal: wp(10)
+    }
+})
