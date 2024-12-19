bf6bbaf60a4953372367170663abe6a58c73ab19
alhwyn <alhwyndwyne0411@gmail.com>
2024-12-19 05:32:50 -0800

    i add a button

---
+++ photobomb/app/lobby.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Button from '../components/Button'
+
+
+const lobby = () => {
+  return (
+    <View>
+      <Text>lobby</Text>
+      <Button title="welcome"/>
+    </View>
+  )
+}
+
+export default lobby
+
+const styles = StyleSheet.create({})
