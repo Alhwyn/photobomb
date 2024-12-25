61e79f22fc77a8fe41072c0a8c2490d6faa82e85
alhwyn <alhwyndwyne0411@gmail.com>
2024-12-25 11:04:40 -0800

    creating the files of the game

---
+++ photobomb/app/games/MainGame.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainGame = () => {
+  return (
+    <View>
+      <Text>MainGame</Text>
+    </View>
+  )
+}
+
+export default MainGame
+
+const styles = StyleSheet.create({})
