433a47677e3a39fea6f4c01c6db5859e2af44023
alhwyn <alhwyndwyne0411@gmail.com>
2025-02-26 13:07:17 -0800

    test

--- photobomb/app/joinGame.jsx
+++ photobomb/app/joinGame.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, Pressable, ImageBackground, TouchableWithoutFeedback, Keyboard, SafeAreaView, Alert } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { theme } from '../constants/theme';
 import BackButton from '../components/BackButton';
 import NumberInput from '../components/NumberInput';
